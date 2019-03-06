5c18c79dc9588216e202eb8116f12f94ae8345f9
alabtsai <t113966@mail.fgu.edu.tw>
2019-03-06 23:07:35 +0800

    tensor_board_fun git add my_tensor_board.py my_tensor_board.py

---
+++ my_tensor_board.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+a=tf.constant(2)
+b=tf.constant(3)
+x=tf.add(a,b)
+
+
+writer = tf.summary.FileWriter('./graphs1', tf.get_default_graph() )
+with tf.Session() as sess:
+    print(sess.run(x))
+writer.close()
